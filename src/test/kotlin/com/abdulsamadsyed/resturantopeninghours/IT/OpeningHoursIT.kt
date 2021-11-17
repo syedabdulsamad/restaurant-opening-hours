7af021450799b77bf8aa158f0b6ae377c9c61afb
Abdul <syed.samad@op.fi>
2021-11-18 00:04:06 +0200

    Exception handling structure. Closes #5

--- src/test/kotlin/com/abdulsamadsyed/resturantopeninghours/IT/OpeningHoursIT.kt
+++ src/test/kotlin/com/abdulsamadsyed/resturantopeninghours/IT/OpeningHoursIT.kt
@@ -1,8 +1,5 @@
 package com.abdulsamadsyed.resturantopeninghours.IT
 
-import com.abdulsamadsyed.resturantopeninghours.ResturantOpeningHoursApplication
-import com.abdulsamadsyed.resturantopeninghours.ResturantOpeningHoursApplicationTests
-import com.abdulsamadsyed.resturantopeninghours.controller.OpeningHoursController
 import com.abdulsamadsyed.resturantopeninghours.service.OpeningHoursService
 import com.abdulsamadsyed.resturantopeninghours.transformer.InputRequestTransformer
 import com.abdulsamadsyed.resturantopeninghours.transformer.OutputResultTransform
@@ -11,21 +8,17 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity.get
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.ResultMatcher
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-//@ExtendWith(SpringExtension::class)
-//@WebMvcTest(controllers = [OpeningHoursController::class])
+// @ExtendWith(SpringExtension::class)
+// @WebMvcTest(controllers = [OpeningHoursController::class])
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
@@ -46,19 +39,19 @@ class OpeningHoursIT {
     @DisplayName("Test the endpoint with request to get opening hours")
     fun testGetOpeningHours() {
         val expectedResult = "Monday: Closed\n" +
-                "Tuesday: 10 AM - 6 PM\n" +
-                "Wednesday: Closed\n" +
-                "Thursday: 10:30 AM - 6 PM, 7 PM - 11 PM\n" +
-                "Friday: 10 AM - 1 AM\n" +
-                "Saturday: 10 AM - 1 AM\n" +
-                "Sunday: 12 PM - 9 PM, 11 PM - 6 PM"
+            "Tuesday: 10 AM - 6 PM\n" +
+            "Wednesday: Closed\n" +
+            "Thursday: 10:30 AM - 6 PM, 7 PM - 11 PM\n" +
+            "Friday: 10 AM - 1 AM\n" +
+            "Saturday: 10 AM - 1 AM\n" +
+            "Sunday: 12 PM - 9 PM, 11 PM - 6 PM"
 
-
-        val result = mockMvc.perform(MockMvcRequestBuilders.get("/opening-hours")
+        val result = mockMvc.perform(
+            MockMvcRequestBuilders.get("/opening-hours")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody())
         ).andExpect(status().isOk)
-                .andReturn()
+            .andReturn()
         assertEquals(result.response.contentAsString, expectedResult)
     }
 
